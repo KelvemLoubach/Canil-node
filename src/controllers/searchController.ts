fb08b6d331ced5103617cedcc45fba4947fa5711
Kelvem Loubach <kelvemloubach@outlook.com>
2023-01-31 15:03:56 -0300

    Final do projeto!

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from "express";
 
+import { getPet } from '../model/pets';
+import { creatMenuObject } from '../hellpers/CreatMenuObject';
+
+
  export const search = ( req: Request, res: Response)=>{
-    res.send('Oi, search aqui')
+
+  let query: string = req.query.name as string;
+
+  if(!query){
+   res.redirect('/')
+   return;
+  };
+
+  let list = getPet.getFromName(query);
+
+    res.render('pages/page', {
+      menu: creatMenuObject(''),
+      list,
+      query
+    })
  } 
